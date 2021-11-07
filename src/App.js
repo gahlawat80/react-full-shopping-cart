d2b34f9cdf2e38c754517646839ea4f85e891e8f
Pardeep Gahlawat <gahlawat80@hotmail.com>
2021-11-06 21:36:16 -0400

    updated project by removing unnecessary files

--- src/App.js
+++ src/App.js
@@ -4,20 +4,15 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="grid-header">
+        <a href="#">Shopping Cart</a>
       </header>
+      <section className="main">
+        Main section
+      </section>
+      <footer className="footer">
+        All rights reserved.
+      </footer>
     </div>
   );
 }
